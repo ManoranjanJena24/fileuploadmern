321a6e0a11dd6346e3a3f37251660570b3a144b6
Manoranjan Jena <manoranjanjenamitsy17@gmail.com>
2025-08-27 14:29:56 +0530

    Done with uploading any file pdf jpg and all any kind of file

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const multer=require('multer')
+const path=require('path')
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Set EJS as template engine
+app.set("view engine", "ejs");
+
+const storage = multer.diskStorage({
+  destination: (req,file,cb) => {
+    cb(null,'./uploads')
+  },
+  filename: (req,file,cb) => {
+    const newFileName=Date.now() + path.extname(file.originalname)
+    cb(null,newFileName)
+  },
+});
+
+
+const upload = multer({
+    storage:storage,
+    limits:{
+        fileSize:1024*1024*3,
+    }
+})
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("myform"); // Looks for views/myform.ejs
+});
+
+app.post("/submitform", upload.single('userfile'),(req,res)=>{
+    res.send(req.file)
+});
+
+// Start server
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
